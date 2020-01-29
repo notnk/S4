24a88807b8b83d8fb3f51ab9253f50c6ea9e8a5a
Navneeth Krishna <34378434+notnk@users.noreply.github.com>
2020-01-29 11:59:01 +0530

    Update 1.cpp

--- OOT/exp1/1.cpp
+++ OOT/exp1/1.cpp
@@ -2,12 +2,20 @@
 using namespace std;
 void sort(int a[][100],int n=3)
 {
+	cout<<"Enter the elements of the array:\n";
+	for(int i=0;i<n;i++)
+	{
+		for(int j=0;j<n;j++)
+		{
+			cin>>a[i][j];
+		}
+	}
 	int temp,i;
 	for(int k=0;k<n;k++)
 	{
 		for( i=0;i<n;i++)
 		{
-			for(int j=0;j<n-j-1;j++)
+			for(int j=0;j<n-i-1;j++)
 			{
 				if(a[k][j]>a[k][j+1])
 				{
@@ -19,20 +27,12 @@ void sort(int a[][100],int n=3)
 		}
 	}		
 }	
-int main()
+main()
 {
 	int a[100][100],n=3;
 	char c;
 	cout<<"Do you want to enter the size(y/n):";
 	cin>>c;
-	cout<<"Enter the elements of the array:\n";
-	for(int i=0;i<n;i++)
-	{
-		for(int j=0;j<n;j++)
-		{
-			cin>>a[i][j];
-		}
-	}
 	switch(c)
 	{
 		case 'y':	cout<<"Enter the size:";
