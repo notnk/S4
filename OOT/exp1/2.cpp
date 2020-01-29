04f9779f10005eb44885485481b4bcfb5e5f4ced
Navneeth Krishna <34378434+notnk@users.noreply.github.com>
2020-01-29 11:35:12 +0530

    Add files via upload

---
+++ OOT/exp1/2.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+float calc(float p,float r,float n,float t)
+{
+	return (p*(1+(r/n))*n*t);
+}
+main()
+{
+	float I,p,r,n,t;
+	cout<<"Enter the principle amount:";
+	cin>>p;
+	cout<<"Enter the rate of interset:";
+	cin>>r;
+	cout<<"Enter the n:";
+	cin>>n;
+	cout<<"Enter the time period:";
+	cin>>t;
+	cout<<"The intreset rate is "<<calc(p,r,n,t);
+}
